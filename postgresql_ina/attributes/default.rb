fe1a65969844fc85706727ddd6bc36a2d7cc14dd
andromedek <kahina.ferroukhi@gmail.com>
2018-07-26 23:21:44 +0200

    new cookbook postgres

---
+++ postgresql_ina/attributes/default.rb
@@ -0,0 +1,6 @@
+default['postgresql']['version']='10'
+default['postgresql']['encoding']='UTF-8'
+default['postgresql']['locale']='UTF-8'
+default['postgresql']['template']='template0'
+default['postgresql']['database']='test_1'
+
---
+++ postgresql_ina/metadata.rb
@@ -0,0 +1,21 @@
+name 'postgresql_ina'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'All Rights Reserved'
+description 'Installs/Configures postgresql_ina'
+long_description 'Installs/Configures postgresql_ina'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
+depends 'postgresql'
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/postgresql_ina/issues'
+
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/postgresql_ina'
